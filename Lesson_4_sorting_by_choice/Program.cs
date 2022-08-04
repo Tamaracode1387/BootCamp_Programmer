e2bc05dbbf46b48219247ccaf88f4ef487dd079f
Tamara <choodo18@rambler.ru>
2022-08-04 23:39:54 +0700

    Написание кода

--- Lesson_4_sorting_by_choice/Program.cs
+++ Lesson_4_sorting_by_choice/Program.cs
@@ -23,3 +23,30 @@ MIN = 15
 15 > 9
 [-3, 2, 6, 9, 15]
 */
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// Заполнение массива
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) +"]");
+// Сортировка
+for (int i = 0; i < n - 1; i++)
+{
+    int MinIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[MinIndex])
+            MinIndex = j;
+    }
+    int temp;
+    temp = array[MinIndex];
+    array[MinIndex] = array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) +"]");
