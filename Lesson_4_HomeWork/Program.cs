2538b9335f4a4f928cc239ec391fc274544ec9a6
Tamara <choodo18@rambler.ru>
2022-08-08 20:36:05 +0700

    Задача СУПЕРСДВИГ

---
+++ Lesson_4_HomeWork/Program.cs
@@ -0,0 +1,39 @@
+﻿// Дана последовательность из N целых чисел и число K. Необходимо сдвинуть всю последовательность 
+// (сдвиг - циклический) на |K| элементов вправо, 
+// если K – положительное и влево, если отрицательное.
+// 	5
+// 5 3 7 4 6
+// 3
+// 7 4 6 5 3
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int[] b = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Введите число К для сдвига: ");
+int k = Convert.ToInt32(Console.ReadLine());
+if (k > 0)
+{
+for (int i = 0; i < k; i++) 
+    b[i] = a[n - k + i];     
+for (int i = 0; i < n - k; i++)
+    b[k + i] = a[i];
+for (int i = 0; i < n; i++)
+    Console.Write(b[i] + " ");
+}
+else
+{
+k = -k;
+for (int i = 0; i < k; i++)
+    b[n - k + i] = a[i];
+for (int i = 0; i < n - k; i++)
+    b[i] = a[k + i];
+for (int i = 0; i < n; i++)
+    Console.Write(b[i] + " ");
+}
